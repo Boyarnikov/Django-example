d12563ae19f58e49e9feecd12a80db11d5473bc5
Boyar <boiarnikov.ia@phystech.edu>
2024-04-19 21:01:34 +0300

    update logic with objects

--- mysite/vote/urls.py
+++ mysite/vote/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "vote"
 urlpatterns = [
-    path('', views.example, name="vote"),
-    path('2', views.example2, name="vote2")
+    path('', views.index, name="index"),
+    path('<int:_id>/', views.question, name="question"),
+    path('<int:_id>/result', views.result, name="result"),
+    path('<int:_id>/vote', views.vote, name="vote")
 ]
--- mysite/vote/views.py
+++ mysite/vote/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.db.models import F
 
+from .models import Question, Vote
 
 # Create your views here.
 
-def example(request):
-    return HttpResponse("Hello world")
+def index(request):
+    data = Question.objects.all()
+    return render(request, "vote/index.html", {"q_list": data})
 
 
-def example2(request):
-    return HttpResponse("Hi world")
+def question(request, _id):
+    data = get_object_or_404(Question, pk=_id)
+    return render(request, "vote/question.html", {"q": data})
+
+
+def result(request, _id):
+    data = get_object_or_404(Question, pk=_id)
+    return render(request, "vote/results.html", {"q": data})
+
+
+def vote(request, _id):
+    data = get_object_or_404(Question, pk=_id)
+    choice = get_object_or_404(Vote, pk=request.POST["vote"])
+    choice.count = F("count") + 1
+    choice.save()
+    return HttpResponseRedirect(reverse("vote:result", args=(data.id, )))
